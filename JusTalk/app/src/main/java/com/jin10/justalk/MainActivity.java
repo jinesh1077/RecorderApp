f588789bdd3d612eb07593bcca0d33c02aba3fff
jinesh <jinesh1077@gmail.com>
2019-04-04 18:17:20 +0530

    1st commit

---
+++ JusTalk/app/src/main/java/com/jin10/justalk/MainActivity.java
@@ -0,0 +1,38 @@
+package com.jin10.justalk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button login,register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login=(Button)findViewById(R.id.login_btn);
+        register=(Button)findViewById(R.id.register_btn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
